97c341e1bcc9f779cfb2dcba87810c449a35d00a
Michal Kvasničák <michal.kvasnicak@gmail.com>
2017-08-25 14:18:50 +0200

    fix code style in test

--- src/index.test.js
+++ src/index.test.js
@@ -16,11 +16,19 @@ Test.contextTypes = contextTypes;
 
 describe('ErrorReporter', () => {
   it('provides captureException to context', () => {
-    const wrapper = mount(<ErrorReporter captureException={() => {}}><Test /></ErrorReporter>);
+    const wrapper = mount(
+      <ErrorReporter captureException={() => {}}>
+        <Test />
+      </ErrorReporter>,
+    );
   });
 
   it('renders all children', () => {
-    const wrapper = mount(<ErrorReporter captureException={() => {}}><Test /></ErrorReporter>);
+    const wrapper = mount(
+      <ErrorReporter captureException={() => {}}>
+        <Test />
+      </ErrorReporter>,
+    );
 
     expect(wrapper.find('div').length).toBe(1);
   });
